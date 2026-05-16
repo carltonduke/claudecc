051afb1a0ba54e0087311b791a1453e6d0080fbd
Carlton Duke <carltondukeatx@gmail.com>
2026-05-16 09:55:14 -0500

    migrate to NeoForge event API

--- forge/src/main/java/dev/carlt/claudecc/forge/ClaudeCCForge.java
+++ forge/src/main/java/dev/carlt/claudecc/forge/ClaudeCCForge.java
@@ -3,19 +3,19 @@ package dev.carlt.claudecc.forge;
 import dan200.computercraft.api.ComputerCraftAPI;
 import dev.carlt.claudecc.ClaudeAPI;
 import dev.carlt.claudecc.ClaudeCommand;
-import net.minecraftforge.event.RegisterCommandsEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.RegisterCommandsEvent;
 
 @Mod("claudecc")
-@Mod.EventBusSubscriber
 public class ClaudeCCForge {
-    public ClaudeCCForge() {
+    public ClaudeCCForge(IEventBus modBus) {
         ComputerCraftAPI.registerAPIFactory(ClaudeAPI::new);
+        NeoForge.EVENT_BUS.addListener(ClaudeCCForge::onRegisterCommands);
     }
 
-    @SubscribeEvent
-    public static void onRegisterCommands(RegisterCommandsEvent event) {
+    private static void onRegisterCommands(RegisterCommandsEvent event) {
         ClaudeCommand.register(event.getDispatcher());
     }
 }
